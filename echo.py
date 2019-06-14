f7272061d4e5c6931f848567cce5efc61986820e
Eileen <eileen.tallman@gmail.com>
2019-06-14 13:45:58 -0400

    Completed -- ready for review

--- echo.py
+++ echo.py
@@ -8,36 +8,46 @@ import unittest
 import sys
 import argparse
 
+def toUpper(str):
+    print(str.upper)
+    return str.upper()
+
+def toLower(str):
+    print(str.lower)
+    return str.lower()
+
+def toTitle(str):
+    print(str.title)
+    return str.title()
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    parser = argparse.ArgumentParser()
-#     parser.parse_args()
-#     #args = parser.parse_args()
-
-#     parser = argparse.ArgumentParser(
-#         description='Perform transformation on input text.')
-#     parser.add_argument("text", help="text to be manipulated")
-#     parser.add_argument("-h", "--help", 
-#                         help="show this help message and exit",)
-#     parser.add_argument( "-u", "--upper", 
-#                         help="convert text to uppercase",)
-#     parser.add_argument("-l", "--lower", 
-#                         help="convert text to lowercase",)
-#     parser.add_argument("-t", "--title", 
-#                         help="convert text to titlecase",
-#                         action="store_true")
-    
-#     my_namespace = parser.parse_args()
-#     print(my_namespace.my_optional)
-    
-# print(create_parser())
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text.')
+    parser.add_argument("text", help="text to be manipulated")
+    parser.add_argument("-u", "--upper",
+                        help="convert text to uppercase", action="store_true")
+    parser.add_argument("-l", "--lower",
+                        help="convert text to lowercase", action="store_true")
+    parser.add_argument("-t", "--title",
+                        help="convert text to titlecase",
+                        action="store_true")
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
-
-#if __name__ == '__main__':
+    parser = create_parser()
+    ns = parser.parse_args(args)
     
-#     # unittest.main()
+    text = ns.text
+    if ns.lower:
+        text = text.lower()
+    if ns.upper:
+        text = text.upper()
+    if ns.title:
+        text = text.title()
+    return text
+
+if __name__ == '__main__':
+    print(main(sys.argv[1:]))
--- test_echo.py
+++ test_echo.py
@@ -1,14 +1,47 @@
-# import unittest
+import echo
+import unittest
+import subprocess
 
-# def test_help(self):
-#     """ Running the program without arguments should show usage. """
+class TestMethods(unittest.TestCase):
+    
+    def setUp(self):
+        self.parser = echo.create_parser()
+    
+    def test_parser(self):
+        args = ["--lower", "--upper", "--title", "HELLO"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.lower)
+        self.assertTrue(ns.upper)
+        self.assertTrue(ns.title)
+        self.assertTrue(ns.text)
+        self.assertEquals(echo.main(args), "Hello")
+        
+    #Tests below call main and compares them to the string in the second argument.
+    def test_lower(self):
+        """ Running the program should show a lowercase string. """
+        args = ["--lower", "HELLO"]
+        self.assertEquals(echo.main(args), "hello")
+        
+    def test_upper(self):
+        """ Running should show an uppercase string. Calls main and compares it to the string in the second argument. """
+        args = ["--upper", "hello"]
+        self.assertEquals(echo.main(args), "HELLO")
 
-#     # Run the command `python ./echo.py -h` in a separate process, then
-#     # collect it's output.
-#     process = subprocess.Popen(
-#         ["python", "./echo.py", "-h"],
-#         stdout=subprocess.PIPE)
-#     stdout, _ = process.communicate()
-#     usage = open("./USAGE", "r").read()
+    def test_title(self):
+        """ Running the program should show a titlecase string. """
+        args = ["--title", "hello"]
+        self.assertEquals(echo.main(args), "Hello")
+        
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
+        # Run the command `python ./echo.py -h` in a separate process, then
+        # collect it's output.
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+        self.assertEquals(stdout, usage)
 
-#     self.assertEquals(stdout, usage)
+if __name__ == '__main__':
+    unittest.main()
